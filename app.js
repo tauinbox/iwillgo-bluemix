21be6e67ee5a4d0ce317c8b0d9e4c5ca2f6e1c1b
Alexander Tupavov <tauinbox@gmail.com>
2016-08-16 18:12:18 +0300

    log info after successful connection to mongodb

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   // we're connected
-  console.log('Connected correctly to server');
+  console.log('Connected correctly to mongodb server');
 });
 
 
