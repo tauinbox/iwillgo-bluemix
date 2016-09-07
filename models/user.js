300828147955ff0b55ea9e927a8e9d359a29b3ee
Alexander Tupavov <tauinbox@gmail.com>
2016-09-07 17:49:40 +0300

    revise Used schema

--- models/user.js
+++ models/user.js
@@ -3,38 +3,19 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-  username: String,
+  username: { type: String, unique: true, required: true },
   password: String,
   OauthId: String,
   OauthToken: String,
-  firstname: {
-    type: String,
-    default: ''
-  },
-  lastname: {
-    type: String,
-    default: ''
-  },  
-  admin:   {
-    type: Boolean,
-    default: false
-  },
-  email: {
-    type: String,
-    default: ''
-  },
-  userpic: {
-    type: String,
-    default: ''
-  },
-  dateOfBirth: {
-    type: Date,
-  },
-  friends: {
-    type: [Schema.Types.ObjectId],
-    ref: 'User'
-  }
-});
+  status: String,
+  firstname: { type: String, default: '' },
+  lastname: { type: String, default: '' },
+  admin:   { type: Boolean, default: false },
+  email: { type: String, default: '' },
+  userpic: { type: String, default: '' },
+  dateOfBirth: { type: Date },
+  friends: { type: [Schema.Types.ObjectId], ref: 'User' }
+}, { timestamps: true });
 
 User.methods.getName = function() {
   return (this.firstname + ' ' + this.lastname);
