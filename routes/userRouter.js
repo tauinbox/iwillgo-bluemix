847bb0e27dd8ed37c506c425021df50126c110eb
Alexander Tupavov <tauinbox@gmail.com>
2016-09-09 11:27:56 +0300

    fixed logoff error

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -18,64 +18,6 @@ router.route('/')
 })
 ;
 
-router.route('/:userId')
-.get(Verify.verifyOrdinaryUser, function(req, res, next) {
-  User.findById(req.params.userId, function(err, user) {
-    if (err) return next(err);
-    // console.log(user);
-    if (req.params.userId != req.decoded._id) {
-      var err = new Error('You are not authorized to perform this operation!');
-      err.status = 403;
-      return next(err);
-    }    
-    res.json(user);
-  });
-})
-
-.put(Verify.verifyOrdinaryUser, function(req, res, next) {
-  User.findById(req.params.userId, function(err, user) {
-    if (err) return next(err);
-    if (req.params.userId != req.decoded._id) {
-      var err = new Error('You are not authorized to perform this operation!');
-      err.status = 403;
-      return next(err);
-    }
-    // update fields from req.body
-    user.firstname = req.body.firstname;
-    user.lastname = req.body.lastname;
-    user.email = req.body.email;
-    user.status = req.body.status;
-
-    user.save(function (err, user) {
-      if (err) return next(err);
-      console.log('User updated!');
-      res.json(user);
-    });    
-  });
-})
-;
-
-router.route('/:userId/friends')
-.get(Verify.verifyOrdinaryUser, function(req, res, next) {
-  User.findById(req.params.userId, function(err, user) {
-    if (err) return next(err);
-    res.json(user.friends);
-  });
-})
-
-.post(Verify.verifyOrdinaryUser, function(req, res, next) {
-  User.findById(req.params.userId, function(err, user) {
-    if (err) return next(err);
-    user.friends.push(req.body);
-    user.save(function(err, user) {
-      if (err) return next(err);
-      console.log('Added new friend!');
-      res.json(user);
-    });    
-  });
-})
-;
-
 router.post('/register', function(req, res) {
   User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
     if (err) return res.status(500).json({err: err});
@@ -140,5 +82,63 @@ router.get('/logout', function(req, res) {
 //     });
 //   })(req, res, next);
 // });
+
+router.route('/:userId/friends')
+.get(Verify.verifyOrdinaryUser, function(req, res, next) {
+  User.findById(req.params.userId, function(err, user) {
+    if (err) return next(err);
+    res.json(user.friends);
+  });
+})
+
+.post(Verify.verifyOrdinaryUser, function(req, res, next) {
+  User.findById(req.params.userId, function(err, user) {
+    if (err) return next(err);
+    user.friends.push(req.body);
+    user.save(function(err, user) {
+      if (err) return next(err);
+      console.log('Added new friend!');
+      res.json(user);
+    });    
+  });
+})
+;
+
+router.route('/:userId')
+.get(Verify.verifyOrdinaryUser, function(req, res, next) {
+  User.findById(req.params.userId, function(err, user) {
+    if (err) return next(err);
+    // console.log(user);
+    if (req.params.userId != req.decoded._id) {
+      var err = new Error('You are not authorized to perform this operation!');
+      err.status = 403;
+      return next(err);
+    }    
+    res.json(user);
+  });
+})
+
+.put(Verify.verifyOrdinaryUser, function(req, res, next) {
+  User.findById(req.params.userId, function(err, user) {
+    if (err) return next(err);
+    if (req.params.userId != req.decoded._id) {
+      var err = new Error('You are not authorized to perform this operation!');
+      err.status = 403;
+      return next(err);
+    }
+    // update fields from req.body
+    user.firstname = req.body.firstname;
+    user.lastname = req.body.lastname;
+    user.email = req.body.email;
+    user.status = req.body.status;
+
+    user.save(function (err, user) {
+      if (err) return next(err);
+      console.log('User updated!');
+      res.json(user);
+    });    
+  });
+})
+;
 
 module.exports = router;
